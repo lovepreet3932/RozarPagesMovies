fb22b0190f3b4b71f99c233b44a7bcd54b4d51a3
Lovepreet Singh <LS052@atcltd.atc.org.nz>
2021-02-25 15:19:44 +1300

    Add project files.

---
+++ Data/RozarPagesMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RozarPagesMovies.Models;
+
+namespace RozarPagesMovies.Data
+{
+    public class RozarPagesMoviesContext : DbContext
+    {
+        public RozarPagesMoviesContext (DbContextOptions<RozarPagesMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RozarPagesMovies.Models.Movie> Movie { get; set; }
+    }
+}
